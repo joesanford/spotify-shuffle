ac0edc99c67e76dd9505c08308e41eb8978f16e8
Joe Sanford <josephsanford@gmail.com>
2024-09-03 00:05:18 -0400

    black run

--- shuffle.py
+++ shuffle.py
@@ -2,7 +2,7 @@ import click
 
 import spotify_utils as su
 
-PLAYLIST_ID = '2uN3clGz8OFuoc6JjOwdNY'
+PLAYLIST_ID = "2uN3clGz8OFuoc6JjOwdNY"
 
 
 @click.command()
@@ -10,12 +10,12 @@ def sync():
     sp = su.init_sp()
 
     saved_tracks = su.get_saved_tracks(sp)
-    print(f'{len(saved_tracks)} saved tracks')
+    print(f"{len(saved_tracks)} saved tracks")
 
     playlist_tracks = su.get_playlist_tracks(sp, PLAYLIST_ID)
-    print(f'{len(playlist_tracks)} playlist tracks')
+    print(f"{len(playlist_tracks)} playlist tracks")
 
-    print('done syncing!')
+    print("done syncing!")
 
 
 @click.command()
@@ -23,11 +23,11 @@ def shuffle():
     sp = su.init_sp()
 
     saved_tracks = su.get_saved_tracks(sp)
-    print(f'{len(saved_tracks)} saved tracks')
+    print(f"{len(saved_tracks)} saved tracks")
 
     su.shuffle_playlist(sp, saved_tracks, PLAYLIST_ID)
 
-    print('done shuffling!')
+    print("done shuffling!")
 
 
 @click.group()
@@ -38,5 +38,5 @@ def cli():
 cli.add_command(sync)
 cli.add_command(shuffle)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
--- spotify_utils.py
+++ spotify_utils.py
@@ -4,109 +4,118 @@ import random
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyOAuth
 
-CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
-CLIENT_SECRET = os.getenv('SPOTIFY_SECRET')
-REDIRECT_URI = 'http://127.0.0.1:8888'
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_SECRET")
+REDIRECT_URI = "http://127.0.0.1:8888"
 CHUNK_SIZE = 100
 
 
 def init_sp() -> Spotify:
-    sp_oauth=SpotifyOAuth(
+    sp_oauth = SpotifyOAuth(
         client_id=CLIENT_ID,
         client_secret=CLIENT_SECRET,
         redirect_uri=REDIRECT_URI,
-        scope='user-library-read, playlist-read-private, playlist-modify-private, playlist-modify-public',
+        scope="user-library-read, playlist-read-private, playlist-modify-private, playlist-modify-public",
         open_browser=False,
     )
 
     token_info = sp_oauth.get_cached_token()
-    
-    if sp_oauth.is_token_expired(token_info):
-        token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
 
-    sp = Spotify(auth=token_info['access_token'])
+    if sp_oauth.is_token_expired(token_info):
+        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
+
+    sp = Spotify(auth=token_info["access_token"])
 
     return sp
+
 
 def print_progress(songs: list, res):
     if len(songs) % 250 == 0:
         print(f'{round(len(songs) / res.get("total") * 100, 1)}% fetched')
 
-    if not res.get('next'):
-        print('100% fetched')
+    if not res.get("next"):
+        print("100% fetched")
 
 
 def get_saved_tracks(sp: Spotify) -> list:
-    print('fetching saved tracks')
+    print("fetching saved tracks")
     songs = []
     offset = 0
 
     while True:
         res = sp.current_user_saved_tracks(limit=50, offset=offset)
-        songs.extend([s.get('track').get('uri') for s in res.get('items')])
+        songs.extend([s.get("track").get("uri") for s in res.get("items")])
         offset += 50
 
         print_progress(songs, res)
 
-        if not res.get('next'):
+        if not res.get("next"):
             break
-    
+
     return songs
 
 
 def get_playlist_tracks(sp: Spotify, playlist: str) -> list:
-    print('fetching playlist tracks')
+    print("fetching playlist tracks")
     songs = []
     offset = 0
 
     while True:
         res = sp.playlist_items(playlist, limit=50, offset=offset)
-        songs.extend([s.get('track').get('uri') for s in res.get('items')])
+        songs.extend([s.get("track").get("uri") for s in res.get("items")])
         offset += 50
 
         print_progress(songs, res)
 
-        if not res.get('next'):
+        if not res.get("next"):
             break
-    
+
     return songs
 
 
-def sync_playlists(sp: Spotify, saved_tracks: list, playlist_tracks: list, destination_playlist: str):
-    print('syncing playlists')
+def sync_playlists(
+    sp: Spotify, saved_tracks: list, playlist_tracks: list, destination_playlist: str
+):
+    print("syncing playlists")
     songs_to_add = [t for t in saved_tracks if t not in playlist_tracks]
     songs_to_remove = [t for t in playlist_tracks if t not in saved_tracks]
 
-    print(f'{len(songs_to_add)} songs to add')
-    print(f'{len(songs_to_remove)} songs to remove')
+    print(f"{len(songs_to_add)} songs to add")
+    print(f"{len(songs_to_remove)} songs to remove")
 
     if songs_to_add:
         sp.playlist_add_items(destination_playlist, songs_to_add)
     if songs_to_remove:
-        sp.playlist_remove_all_occurrences_of_items(destination_playlist, songs_to_remove)
+        sp.playlist_remove_all_occurrences_of_items(
+            destination_playlist, songs_to_remove
+        )
 
     return
 
 
 def shuffle_playlist(sp: Spotify, tracks: list, playlist: str):
-    print('shuffling playlist')
+    print("shuffling playlist")
     random.shuffle(tracks)
 
     tracks_to_delete = tracks[:]
 
     total_tracks_to_delete = len(tracks_to_delete)
     while tracks_to_delete:
-        sp.playlist_remove_all_occurrences_of_items(playlist, tracks_to_delete[:CHUNK_SIZE])
+        sp.playlist_remove_all_occurrences_of_items(
+            playlist, tracks_to_delete[:CHUNK_SIZE]
+        )
         del tracks_to_delete[:CHUNK_SIZE]
-        print(f'{round(100 - len(tracks_to_delete) / total_tracks_to_delete * 100, 1)}% deleted')
+        print(
+            f"{round(100 - len(tracks_to_delete) / total_tracks_to_delete * 100, 1)}% deleted"
+        )
 
-    print('done deleting')
-    
+    print("done deleting")
+
     total_tracks = len(tracks)
-    print(f'{total_tracks} to add')
+    print(f"{total_tracks} to add")
     while tracks:
         sp.playlist_add_items(playlist, tracks[:CHUNK_SIZE])
         del tracks[:CHUNK_SIZE]
-        print(f'{round(100 - len(tracks) / total_tracks * 100, 1)}% added')
+        print(f"{round(100 - len(tracks) / total_tracks * 100, 1)}% added")
 
-    print('done repopulating')
+    print("done repopulating")
